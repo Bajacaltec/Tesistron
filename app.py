ada3c50737f7ba397bbdb2a441f0876f2448ba1e
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-05-14 17:24:15 -0500

    a

--- app.py
+++ app.py
@@ -1,2 +1,39 @@
 import streamlit as st
+import sqlite3
 #APP que permite hacer el análisis para una tesis automaticamente al capturar los datos en la webapp
+st.set_page_config(layout="wide")
+#Usuario y contraseña
+fol1,fol2,fol3,fol4=st.columns(4)
+with fol3:
+    usuario=st.text_input('Usuario')
+with fol4:
+    contraseña=st.text_input('Contraseña')
+with fol1:
+    st.title('Tesistron')
+    
+con = sqlite3.connect('data.db')
+cur = con.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS B
+            (Nombre TEXT,Número INT)""")
+
+col1,col2=st.columns(2)
+with col1:
+    with st.expander('Identificación'):
+        with st.form('Identificación'):
+            nombre=st.text_input('Nombre:')
+            registro=st.form_submit_button('registra',)
+            if registro==True:
+                cur.execute("""INSERT INTO B(Nombre)
+                            VALUES(?)""",nombre)
+                con.commit()
+                con.close()
+                
+        
+with col2:
+    st.subheader('Resumen')
+    con = sqlite3.connect('data.db')
+    cur = con.cursor()
+    sumedad=cur.execute('''Select* FROM B''')
+    resumen=cur.fetchall()
+    st.table(resumen)
+    
