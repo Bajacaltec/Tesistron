0e2d25efa3dd3591254309e7b5f4aab6fb5a08d8
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-05-14 18:00:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from numpy import append
 import streamlit as st
 import sqlite3
+
+from sympy import expand
 #APP que permite hacer el análisis para una tesis automaticamente al capturar los datos en la webapp
 st.set_page_config(layout="wide")
 #Usuario y contraseña
@@ -8,7 +10,7 @@ fol1,fol2,fol3,fol4=st.columns(4)
 with fol3:
     usuario=st.text_input('Usuario')
 with fol4:
-    contraseña=st.text_input('Contraseña')
+    contraseña=st.text_input('Contraseña',type='password')
 with fol1:
     st.title('Tesistron')
     
@@ -17,11 +19,7 @@ cur = con.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS B
             (Nombre TEXT,Número INT)""")
 
-c=['Hola','Bueno']
-agregar=st.text_input('Agregar')
-c.append(agregar)
-for i in c:
-    st.text_input(i)
+
 col1,col2=st.columns(2)
 with col1:
     with st.expander('Identificación'):
@@ -35,10 +33,11 @@ with col1:
                 
         
 with col2:
-    st.subheader('Resumen')
-    con = sqlite3.connect('data.db')
-    cur = con.cursor()
-    sumedad=cur.execute('''Select Nombre FROM B''')
-    resumen=cur.fetchall()
-    st.table(resumen)
+    with st.expander('Resumen',expanded=True):
+        st.subheader('Resumen',)
+        con = sqlite3.connect('data.db')
+        cur = con.cursor()
+        sumedad=cur.execute('''Select Nombre FROM B''')
+        resumen=cur.fetchall()
+        st.table(resumen)
     
