a2ebb8c74e35fffeca9067dc4573c9de37bb39a4
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-05-21 07:49:14 -0500

    a

--- inicio.py
+++ inicio.py
@@ -3,4 +3,7 @@ import streamlit as st
 def inicio():
     st.title('Bienvenido a el tesistron')
     st.success('El mejor apoyo para iniciar, desarrollar, terminar una tésis y finalmente publicar tu trabajo')
+    col1,col2=st.columns(2)
+    with col2:
+        st.video('https://www.youtube.com/watch?v=dsBTlaHxG2Q')
     
